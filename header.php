e5893f32487ddf514c14784d9b2dc3dc4c88274f
Oliver Berghold <info@oliverberghold.de>
2022-01-12 01:26:28 +0100

    style dropdown

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'resonanz' ); ?></button>
+				<button class="button button--menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'resonanz' ); ?></button>
 				<?php
 				wp_nav_menu(
 					array(
