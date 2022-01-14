e6acd01c153e0857d9e8c0aae8755e967be00678
Oliver Berghold <info@oliverberghold.de>
2022-01-14 10:36:18 +0100

    add Add and Remove on Class on Scroll position, depending on percentuage value

--- js/header.js
+++ js/header.js
@@ -5,9 +5,19 @@ const header_height = header.offsetHeight
 const add_class_on_scroll = () => header.classList.add('site-header--shadow')
 const remove_class_on_scroll = () => header.classList.remove('site-header--shadow')
 
+// window.addEventListener('scroll', function() {
+// 	scrollpos = window.scrollY;
+// 	if (scrollpos >= header_height) {
+// 		add_class_on_scroll()
+// 	} else {
+// 			remove_class_on_scroll()
+// 		}
+// 		console.log(scrollpos)
+// })
+
 window.addEventListener('scroll', function() {
 	scrollpos = window.scrollY;
-	if (scrollpos >= header_height) {
+	if (scrollpos >= 10) {
 		add_class_on_scroll()
 	} else {
 			remove_class_on_scroll()
