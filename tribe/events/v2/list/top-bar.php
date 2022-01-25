2a91ae5554d0c30883b4080076d4007fae976059
Oliver Berghold <info@oliverberghold.de>
2022-01-25 16:03:28 +0100

    add template overrides + refactor events header

---
+++ tribe/events/v2/list/top-bar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * View: Top Bar
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/v2/list/top-bar.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link http://evnt.is/1aiy
+ *
+ * @version 5.0.1
+ *
+ */
+?>
